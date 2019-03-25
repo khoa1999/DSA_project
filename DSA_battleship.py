0bd13f223e8b976373874e6ccf0dd71ebbb8e51f
anh56 <48195820+anh56@users.noreply.github.com>
2019-03-25 22:37:18 +0700

    Update DSA_battleship.py

--- DSA_battleship.py
+++ DSA_battleship.py
@@ -13,3 +13,16 @@ class BattleshipGame():
         for i in range(10):
             self.userBoard.append([' '] * 10)
             self.computerBoard.append([' '] * 10)
+    def drawBoard(self, hide):
+        # Draw boards
+        # Aircraft is A, Destroyer is D, Submarine is S, Patrol boat is P, Battleship is B
+        # Rows are denoted from A to J
+        # Columns are denoted from 1 to 10    
+        stats = [['Nbr. of hits  :', self.getHits(True), self.getHits(False)], \
+                 ['Nbr. of misses:', self.getMisses(True), self.getMisses(False)], \
+                 ['Ships sunk    :', len(self.getEnemyFleet(True)[1]), len(self.getEnemyFleet(False)[1])]]
+        
+        
+        
+        
+      
